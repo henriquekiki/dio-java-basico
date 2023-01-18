d27b90bd736ceef5feefb84003dab193f0b96433
henriquekiki <ricckdmo@gmail.com>
2023-01-18 09:20:07 -0300

    Teste

--- meu-primeiro-projeto-java/src/App.java
+++ meu-primeiro-projeto-java/src/App.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Aprendendo JAVA!");
+        
+        System.out.print("Hello Java!");
+ 
+
     }
 }
