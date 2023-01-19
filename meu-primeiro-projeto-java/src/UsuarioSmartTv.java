c312e9390da487a3c0c5d216e9c5149b0b52d75e
henriquekiki <ricckdmo@gmail.com>
2023-01-18 21:06:59 -0300

    teste

---
+++ meu-primeiro-projeto-java/src/UsuarioSmartTv.java
@@ -0,0 +1,34 @@
+public class UsuarioSmartTv {
+    public static void main(String[] args) throws Exception {
+        
+        SmartTv smartTv = new SmartTv();
+
+        System.out.println("TV Ligada ?" + smartTv.ligada);
+        System.out.println("Canal Atual ?" + smartTv.canal);
+        System.out.println("Volume Atual ?" + smartTv.volume);
+
+        smartTv.mudarCanal(27);
+        System.out.println("Canal Atual ?" + smartTv.canal);
+
+        //smartTv.aumentarVolume();
+       //System.out.println("Volume Atual ?" + smartTv.volume);
+
+       //smartTv.diminuirVolume();
+       //smartTv.diminuirVolume();
+       //System.out.println("Volume Atual ?" + smartTv.volume);
+
+       //smartTv.aumentarCanal();
+       //System.out.println("Canal Atual ?" + smartTv.canal);
+
+        //smartTv.diminuirCanal();
+        //System.out.println("Canal Atual ?" + smartTv.canal);
+        
+        //smartTv.ligar ();
+        //System.out.println("Novo status -> TV Ligada ?" + smartTv.ligada);
+        
+        //smartTv.desligar ();
+        //System.out.println("Novo status -> TV Ligada ?" + smartTv.ligada);
+
+
+    }
+}
